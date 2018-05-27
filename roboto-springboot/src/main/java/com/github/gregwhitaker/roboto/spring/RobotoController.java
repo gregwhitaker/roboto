adc70f16f4fdd1bd674969d71ef0825575bcf92b
gregwhitaker <gwhitake@gmail.com>
2018-05-26 19:40:05 -0700

    Doing work

--- roboto-springboot/src/main/java/com/github/gregwhitaker/roboto/spring/RobotoController.java
+++ roboto-springboot/src/main/java/com/github/gregwhitaker/roboto/spring/RobotoController.java
@@ -20,7 +20,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 
